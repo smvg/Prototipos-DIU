767f80abd027a60f9366a0ec1d60fc3d31493ecc
Sam Vargas <samfefuty@hotmail.com>
2019-12-04 17:40:37 +0100

    Cambio codigo por foto en disponibilidad. Esto me sirve de recordatorio: LEE EL ENUNCIADO ANTES DE PASARTE UNA HORA PROGRAMANDO EN JAVASCRIPT.

--- js/disponibilidad.js
+++ js/disponibilidad.js
@@ -1,3 +1,8 @@
-vehiculos = [{id: 3, color:  'lightgray'},{id: 54, color: 'lightgray'}]
+vehiculos = []
+
+for (var x = 0; x < 50; x++) {
+    vehiculos.push({id: x, color:  'lightgray'})
+}
+/*vehiculos = [{id: 3, color:  'lightgray'},{id: 54, color: 'lightgray'}]*/
 
 crearMapa(100, vehiculos);
