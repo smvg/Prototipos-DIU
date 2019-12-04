f51ade83d1cb6ab080884ebc8b2a3bc934419645
Sam Vargas <samfefuty@hotmail.com>
2019-12-04 18:16:39 +0100

    Alertas añadidas al modificar datos

--- js/modificarDatosUsuario.js
+++ js/modificarDatosUsuario.js
@@ -47,3 +47,24 @@ jQuery(function ($) {
   $('.floating-notification').click(function () {
     window.location.href = "notificaciones.html";
   });
+
+  $('#btn-guardar').click(function () {
+    var dni = document.getElementById('dni').value;
+    var telefono = document.getElementById('telefono').value;
+
+    var numeros = dni.substring(0, 8)
+    var letra = dni.substring(8, 9)
+
+    if (dni.length !== 9 || !letra.match(/^[A-Za-z]+$/) || isNaN(numeros)) {
+      alert("El DNI proporcionado no es correcto!")
+      return;
+    }
+    
+    if (isNaN(telefono)) {
+      alert("El teléfono proporcionado no es correcto!")
+      return;
+    }
+
+    alert("Los datos se han guardado correctamente :)")
+
+  });
